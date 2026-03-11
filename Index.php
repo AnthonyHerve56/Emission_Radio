82a56c77a0966c5d44590665992573aff3d0f201
AnthonyHerve56 <herve.anthony.pro@gmail.com>
2026-03-11 10:53:01 +0100

    Changement en php et création page fonction

--- Index.php
+++ Index.php
@@ -1,9 +1,16 @@
+<?php
+    include 'fonction.php';
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="fr">
 
 <head>
     <meta charset="UTF-8">
-    <meta name="Probabilité match" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="Anthony HERVE , Mohamed Ali SHAIEK">
     <title>Accueil</title>
     <link rel="stylesheet" href="./style.css/styles.css">
@@ -13,14 +20,11 @@
     <header>
         <nav class="menu">
             <ul>
-                <li><a href="#Accueil">Accueil</a></li>
+                <li><a href="Index.php">Test2</a></li>
                 <li><a href="Profil.html">Profil</a></li>
             </ul>
         </nav>
     </header>
-    <div>
-
-    </div>
     <h1 style="text-align: center;">Bienvenue sur notre site de prédiction de match de football !</h1>
 
     <div class="accueil-container">
@@ -31,9 +35,7 @@
         <div class="Titre">
             <p class="rainbow-text">Découvrez les dernières tendances et analyses pour vos équipes préférées.</p>
         </div>
-        <div class="ChatFoot">
-            <img src="./images/ChatFoot.webp" alt="ChatFoot" class="ChatFootImage">
-        </div>
+        
     </div>
 
     <footer id="Contact">
