d5e09d1308b3accce1830fc1d3b18334865c630f
AnthonyHerve56 <herve.anthony.pro@gmail.com>
2026-03-25 13:28:13 +0100

    AUrevoire pred

--- Prediction/footer.php
+++
@@ -1,9 +0,0 @@
-<!-- Footer reutilisable : a inclure dans les pages avec include 'footer.php' -->
-
-
-
-<footer id="Contact">
-        <p> Tous droits réservés.</p>
-        <p>Contactez-nous : <a href="mailto:contact@predictionmatch.fr">contact@predictionmatch.fr</a></p>
-        <p>Autheur de ce site : Anthony HERVE et Mohamed Ali SHAIEK</p>
-    </footer>
